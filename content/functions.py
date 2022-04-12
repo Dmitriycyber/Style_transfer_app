a6e2e3fb3127593917f0695b02db2bcfc0493ea4
dimario160517 <nikolaevdn15@gmail.com>
2022-04-12 16:57:59 +0300

    create help-functions

---
+++ content/functions.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+def clip_0_1(image):
+    """
+    Мы хотим уметь отображать нашу полученную картинку, а для этого ее значения должны
+    находится в промежутке от 0 до 1. Наш алгоритм оптимизации этого нигде не учитывает
+    поэтому к полученному изображению мы будем применять "обрезку" по значению
+
+    """
+    return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+
+def load_img(path_to_img, max_dim=512):
+    """
+    Данная функция считывает изображение с диска и приводит его к такому размеру,
+    чтобы бОльшая сторона была равна max_dim пикселей.
+
+    Для считывания изображения воспользуемся функциями tensorflow.
+    """
+    img = tf.io.read_file(path_to_img)  # считываени файла
+    img = tf.image.decode_image(img, channels=3)  # декодинг
+    img = tf.image.convert_image_dtype(img, tf.float32)  # uint8 -> float32, 255 -> 1
+
+    shape = img.numpy().shape[:-1]
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+    new_shape = tuple((np.array(shape) * scale).astype(np.int32))
+
+    img = tf.image.resize(img, new_shape)  # изменение размера
+    img = img[tf.newaxis, :]  # добавляем batch dimension
+    return img
+
+
+def imshow(image, title=None):
+    """
+    Функция для отрисовки изображения
+    """
+    if len(image.shape) > 3:
+        image = tf.squeeze(image, axis=0)
+    plt.axis('off')
+    plt.imshow(image)
+    if title:
+        plt.title(title)
+
+
+def show_pair(original, generated, title=None):
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    imshow(original, 'Original Image')
+    plt.subplot(1, 2, 2)
+    imshow(generated, title)
