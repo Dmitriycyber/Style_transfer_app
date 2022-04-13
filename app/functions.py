b2383a20b8060fca419a97a50b31b087d2b2a85b
dimario160517 <nikolaevdn15@gmail.com>
2022-04-13 12:36:05 +0300

    union some functions

--- app/functions.py
+++ app/functions.py
@@ -2,6 +2,54 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 
+
+class StyleAndContentExtractor:
+    def __init__(self, style_layers, content_layers):
+        self.style_layers = style_layers
+        self.content_layers = content_layers
+        self.vgg_outputs_model = None
+        self.num_style_layers = len(style_layers)
+        self.vgg_outputs_model = get_vgg_layers_model(style_layers + content_layers)
+        self.vgg_outputs_model.trainable = False
+
+    def __call__(self, inputs):
+        preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs * 255.)
+        outputs = self.vgg_outputs_model(preprocessed_input)
+
+        style_outputs_layers = outputs[:self.num_style_layers]
+        content_outputs_layers = outputs[self.num_style_layers:]
+
+        style_outputs = [gram_matrix(style_output) for style_output in style_outputs_layers]
+        content_outputs = [content_output for content_output in content_outputs_layers]
+
+        features_dict = {}
+        features_dict["style"] = {name: value for name, value in zip(self.style_layers, style_outputs)}
+        features_dict["app"] = {name: value for name, value in zip(self.content_layers, content_outputs)}
+
+        return features_dict
+
+
+def style_content_loss(image, style_targets, content_targets, style_weight, content_weight, tv_weight, extractor):
+    style_loss = None
+    content_loss = None
+    features_style = extractor(image)['style']
+    features_content = extractor(image)['content'],
+    content_loss = tf.add_n([tf.keras.losses.MeanSquaredError()(features_content[name], content_targets[name])
+                             for name in features_content.keys()])
+    style_loss = tf.add_n([tf.keras.losses.MeanSquaredError()(features_style[name], style_targets[name])
+                           for name in features_style.keys()])
+    loss = style_weight * style_loss + content_weight * content_loss + tv_weight * tf.image.total_variation(image)
+    return loss
+
+
+def train_step(image, loss_func, optimizer):
+    with tf.GradientTape() as tape:
+        loss = loss_func(image)
+    grad = tape.gradient(loss, image)
+    optimizer.apply_gradients([(grad, image)])
+    image.assign(clip_0_1(image))
+    return loss.numpy()
+
 def clip_0_1(image):
     """
     Мы хотим уметь отображать нашу полученную картинку, а для этого ее значения должны
--- app/transfer.py
+++ app/transfer.py
@@ -4,23 +4,23 @@ from tkinter import ttk
 import pathlib
 from pathlib import Path
 from tkinter.filedialog import askopenfilename
-from app.style_content_extractor import StyleAndContentExtractor
+from app.functions import StyleAndContentExtractor
 from app.functions import load_img, imshow
 
 # style_weight = 100.0
 # content_weight = 5.0
 # tv_weight = 0.1
 
-# style_layers = ['block1_conv1',
-#                 'block2_conv1',
-#                 'block3_conv1',
-#                 'block4_conv1',
-#                 'block5_conv1']
-# content_layers = ['block4_conv2']
-#
-# extractor = StyleAndContentExtractor(style_layers=style_layers, content_layers=content_layers)
-# sample_image = np.ones((1, 512, 512, 3), dtype=np.float32)
-# style_and_content_targets = extractor(sample_image)
+style_layers = ['block1_conv1',
+                'block2_conv1',
+                'block3_conv1',
+                'block4_conv1',
+                'block5_conv1']
+content_layers = ['block4_conv2']
+
+extractor = StyleAndContentExtractor(style_layers=style_layers, content_layers=content_layers)
+sample_image = np.ones((1, 512, 512, 3), dtype=np.float32)
+style_and_content_targets = extractor(sample_image)
 
 # ttk().withdraw()
 content_path = askopenfilename()
@@ -32,9 +32,9 @@ print(style_path)
 content_image = load_img(content_path, 1024)
 style_image = load_img(style_path, 1024)
 
-# style_targets = extractor(style_image)['style']
-# content_targets = extractor(content_image)['content']
-# plt.figure()
-# imshow(style_image, "Style Image")
-# plt.figure()
-# imshow(content_image, "Content Image")
+style_targets = extractor(style_image)['style']
+content_targets = extractor(content_image)['content']
+plt.figure()
+imshow(style_image, "Style Image")
+plt.figure()
+imshow(content_image, "Content Image")
