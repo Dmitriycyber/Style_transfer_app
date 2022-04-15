bb594fd56a2303de1b70cd3877ec9abb7df17257
dimario160517 <nikolaevdn15@gmail.com>
2022-04-15 12:56:32 +0300

    refactoring

--- app/transfer.py
+++ app/transfer.py
@@ -25,7 +25,6 @@ extractor = StyleAndContentExtractor(style_layers=style_layers, content_layers=c
 sample_image = np.ones((1, 512, 512, 3), dtype=np.float32)
 style_and_content_targets = extractor(sample_image)
 
-# ttk().withdraw()
 content_path = askopenfilename()
 print(content_path)
 # content_path = '/moscow-moscow-city-moskva-moskva-siti-gorod-city.jpeg'
