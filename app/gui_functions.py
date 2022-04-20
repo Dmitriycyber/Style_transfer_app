b2fd8e974744e302d224939da5c92e1e5430d248
dimario160517 <nikolaevdn15@gmail.com>
2022-04-20 15:07:11 +0300

    possibility save selected file in misc folder

--- app/gui_functions.py
+++ app/gui_functions.py
@@ -1,13 +1,28 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+import os
+from PIL import Image, ImageDraw
 
-def open_file():
+def open_file_style():
     """Открываем файл для редактирования"""
-    filepath = askopenfilename(
-        filetypes=[("Графические файлы", "*.jpg|*jpeg"), ("Все файлы", "*.*")]
-    )
-    if not filepath:
-        return
-        txt_edit.delete("1.0", tk.END)
-    else:
-        return filepath
+    filepath = askopenfilename(filetypes=[("Графические файлы", "*.jpeg"), ("Все файлы", "*.*")])
+    if not os.path.exists("misc"):
+        os.makedirs("misc")
+    # os.mkdir("Users/user/Desktop/projects/External/style_transfer/misc")
+    img = Image.open(filepath)
+    os.chdir("misc")
+    img.save('style_image.jpg')
+    print(os.getcwd())
+    os.chdir("/Users/user/Desktop/projects/External/style_transfer")
+
+
+
+def open_file_content():
+    """Открываем файл для редактирования"""
+    filepath = askopenfilename(filetypes=[("Графические файлы", "*.jpeg"), ("Все файлы", "*.*")])
+    if not os.path.exists("misc"):
+        os.makedirs("misc")
+    img = Image.open(filepath)
+    os.chdir("misc")
+    img.save('content_image.jpg')
+    os.chdir("/Users/user/Desktop/projects/External/style_transfer")
